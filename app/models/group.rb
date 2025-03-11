4d1497282f1a65cbb71d950274d6531debb7f627
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-03-11 13:16:05 +0200

    update match model to set winner

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+  VALID_TITLES = ["Group A", "Group B", "Group C"]
+
+  validates :title, presence: true, inclusion: { in: VALID_TITLES, message: "%{value} is not a valid group title" }
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,20 @@
 class Match < ApplicationRecord
-  belongs_to :player1
-  belongs_to :player2
-  belongs_to :winner
+  belongs_to :player1, class_name: "User"
+  belongs_to :player2, class_name: "User"
+  belongs_to :winner, class_name: "User", optional: true
   belongs_to :cycle
+
+  after_update :set_winner, if: -> { player1_score.present? && player2_score.present? }
+
+  private
+
+  def set_winner
+    if player1_score > player2_score
+      update_column(:winner_id, player1_id)
+    elsif player2_score > player1_score
+      update_column(:winner_id, player2_id)
+    else
+      update_column(:winner_id, nil) # Handle ties if necessary
+    end
+  end
 end
