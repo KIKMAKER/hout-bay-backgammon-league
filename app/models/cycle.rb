376d01c3dfc7c764a06f107b38a89047777fdfeb
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-06-01 16:15:22 +0200

    dep destroy matches in cycle

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -1,6 +1,6 @@
 class Cycle < ApplicationRecord
   belongs_to :group
-  has_many :matches
+  has_many :matches, dependent: :destroy
   validates :start_date, :weeks, :end_date, presence: true
   validate :end_date_after_start_date
 
@@ -14,4 +14,3 @@ class Cycle < ApplicationRecord
     end
   end
 end
-
