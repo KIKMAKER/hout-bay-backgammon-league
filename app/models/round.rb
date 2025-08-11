07995e6793ec67e3a24216afbb8614b525c3afec
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-08-11 13:43:04 +0200

    validate round end after start

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,6 +1,15 @@
 class Round < ApplicationRecord
   has_many :cycles, dependent: :nullify
   validates :start_date, :end_date, presence: true
+  validate  :end_after_start
+
+  private
+
+  def end_after_start
+    return if start_date.blank? || end_date.blank?
+    errors.add(:end_date, "must be after start date") if end_date <= start_date
+  end
+  
   def name
     "#{start_date.strftime('%d %b %Y')} – #{end_date.strftime('%d %b %Y')}"
   end
